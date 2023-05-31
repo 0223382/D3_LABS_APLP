eb058386b52d43452ac74a4c5ee498b908bf5d9e
Ana Paula Luna <0223382@up.edu.mx>
2023-05-31 17:04:32 -0600

    first commit

---
+++ d3Lab-master -II/d3Lab-master/Template/js/main.js
@@ -0,0 +1,23 @@
+/*
+*    main.js
+*/
+var data = [25, 20, 15, 10, 5];
+var svg = d3.select("#chart-area").append("svg")
+     .attr("width", 400)
+     .attr("height", 400);
+var rect = svg.selectAll("rect")
+     .data(data);
+rect.enter()
+     .append("rect")
+          .attr("x", (d, i)=>{
+               console.log("Item: " + d + "Index: " + i);
+               return (i * 50) + 25;
+          })
+
+          .attr("y", 20)
+
+          .attr("width", 40)
+
+          .attr("height", (d)=>{return d;})
+
+          .attr("fill", "green");
---
+++ d3Lab-master -III/d3Lab-master/Template/js/main.js
@@ -0,0 +1,19 @@
+/*
+*    main.js
+*/
+d3.csv("data/ages.csv").then((data) => {
+
+     console.log(data);
+
+});
+d3.tsv("data/ages.tsv").then((data) => {
+
+     console.log(data);
+
+}); 
+d3.json("data/ages.json").then((data) => {
+
+     console.log(data);
+
+});
+/* fin */
