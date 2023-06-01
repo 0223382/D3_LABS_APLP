ec86e1847383554b7a702b7b6d5b673d4545cadf
Ana Paula Luna <0223382@up.edu.mx>
2023-05-31 19:11:46 -0600

    ejercicios

--- d3Lab-master -III/d3Lab-master/Template/js/main.js
+++ d3Lab-master -III/d3Lab-master/Template/js/main.js
@@ -4,16 +4,24 @@
 d3.csv("data/ages.csv").then((data) => {
 
      console.log(data);
-
+}).catch (function(error) {
+          console.log(error);
 });
 d3.tsv("data/ages.tsv").then((data) => {
 
      console.log(data);
 
-}); 
+}).catch(function (error) {
+     console.log(error);
+}); /* */
 d3.json("data/ages.json").then((data) => {
+
+     data.forEach((d) => {
+
+          d.age = +d.age;
+
+     });
 
      console.log(data);
 
 });
-/* fin */
