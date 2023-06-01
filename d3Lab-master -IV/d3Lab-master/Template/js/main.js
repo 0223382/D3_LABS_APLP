fb4d564ccd8e4a304fcaffba177b829acbcd9529
Ana Paula Luna <0223382@up.edu.mx>
2023-05-31 19:59:08 -0600

    actualizacion 3

--- d3Lab-master -IV/d3Lab-master/Template/js/main.js
+++ d3Lab-master -IV/d3Lab-master/Template/js/main.js
@@ -14,27 +14,12 @@ d3.json("data/buildings.json").then((data) => {
           d.height = +d.height
      });
 
-     var rects = svg.selectAll("rect")
-          .data(data);
-
-     rects.enter()
-          .append("rect")
-          .attr("x", (d, i) => { 
-               return (i * 50) + 20; })
-          .attr("y", 10)
-          .attr("width", 20)
-          .attr("height", (d) => { return d.height;})
-          .attr("fill", "red");
-
+     var namesBld = data.map((d) => { return d.name});
 
      var x = d3.scaleBand()
-
           .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-
           .range([0, 400])
-
           .paddingInner(0.3)
-
           .paddingOuter(0.3);
 
      var y = d3.scaleLinear()
@@ -43,9 +28,26 @@ d3.json("data/buildings.json").then((data) => {
 
      var color = d3.scaleOrdinal()
 
-          .domain(data.lenght)
+          .domain(0, data.lenght)
 
           .range(['turquoise', 'pink', 'orangered', 'royalblue']);
+     
+     
+
+     var rects = svg.selectAll("rect")
+          .data(data);
+
+     rects.enter()
+          .append("rect")
+          .attr("x", (d, i) => {
+               return (i * 50) + 20;
+          })
+          .attr("y", (d) => y(d.height)
+          )
+          .attr("width", 20)
+          .attr("height", (d) => { return d.height; })
+          .attr("fill", (d) => { return color(d.name); });
+
 
 }).catch(function (error) {
      console.log(error);
